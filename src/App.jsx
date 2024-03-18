0a34bce3e1bc3328ae724b11d9579ee12f99bde3
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-18 21:33:24 +0600

    installed tailwind and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <h1 className='text-5xl bg-red-300'>Vite + React</h1>
+      <button className='btn btn-primary'>check</button>
+      
+      
+    </>
+  )
+}
+
+export default App
