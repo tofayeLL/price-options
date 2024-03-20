2ff86303162e663ccfc646bc88782fe1a430a66b
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-20 15:58:46 +0600

    added loading spinner and the spinner get from react awesome components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -2,9 +2,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { RotatingLines } from 'react-loader-spinner'
 
 const Phones = () => {
     const [phones, setPhones] = useState([])
+    const [loadData, setLoadData] = useState(true)
     useEffect(() => {
         /*   fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
               .then(res => res.json())
@@ -26,6 +28,7 @@ const Phones = () => {
                 })
 
                 setPhones(phonesFakeData);
+                setLoadData(false)
             });
 
     }, [])
@@ -36,15 +39,50 @@ const Phones = () => {
 
 
     return (
-        <div>
+        <div >
+
+
+            {/* loading spinner condition  */}
+            {/* <div className="w-[100%] flex justify-center items-center py-10 ">
+                {
+                    loadData ? <RotatingLines  ></RotatingLines>  : ' '
+                }
+            </div> */}
+
+            {/* another condition */}
+
+            {
+                loadData && <div className="w-[100%] flex justify-center items-center">
+                    <RotatingLines
+                        visible={true}
+                        height="200"
+                        width="200"
+                        color="grey"
+                        strokeWidth="5"
+                        animationDuration="0.75"
+                        ariaLabel="rotating-lines-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                    />
+
+                </div>
+            }
+
+
+
+
+
+
+
+
             <h3 className="text-xl font-bold">phones: {phones.length}</h3>
             <BarChart width={1200} height={600} data={phones}>
-                
+
                 <Bar dataKey="price" fill="#8884d8" />
                 <XAxis dataKey='name'></XAxis>
                 <YAxis></YAxis>
                 <Tooltip></Tooltip>
-              
+
             </BarChart>
 
         </div>
